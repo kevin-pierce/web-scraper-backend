368c3abb9bca72ecd6c99d88fd504e7fd0bef52d
Kevin Pierce <kevinpierce08@gmail.com>
2020-08-24 12:35:48 -0400

    Updated search query to look at all specific deals on RedFlagDeals.com under Apparel

--- webscraper.py
+++ webscraper.py
@@ -3,7 +3,16 @@ from bs4 import BeautifulSoup
 
 url = "https://forums.redflagdeals.com/hot-deals-f9/?c=12"
 response = requests.get(url, timeout=5)
-content = BeautifulSoup(response.content, "html.parser")
+soup = BeautifulSoup(response.content, "html.parser")
 
-for deal in content.findAll('li', attrs={"class": "topiclist topics with_categories"}):
-    print (deal.text.encode('utf-8'))
+deals = soup.select('div#site_container div#site_content div#partition_forums div.forums_layout div.primary_content div.forumbg div.inner ul.topiclist.topics.with_categories')
+
+#for deal in deals.findAll('ul', attrs={"class": "topiclist topics with_categories"}):
+#    print(deal)
+
+print (deals)
+
+#div div div div div div ul li'
+
+#for deal in soup.findAll('li', attrs={"class": "topiclist topics with_categories"}):
+#    print (deal.text.encode('utf-8'))
