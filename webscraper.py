b3748802503e6cbf63475616b7d6320c1ce4faf9
Kevin Pierce <kevinpierce08@gmail.com>
2020-08-24 02:44:58 -0400

    Updated scrape query

--- webscraper.py
+++ webscraper.py
@@ -1,3 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://forums.redflagdeals.com/hot-deals-f9/?c=12"
+response = requests.get(url, timeout=5)
+content = BeautifulSoup(response.content, "html.parser")
+
+for deal in content.findAll('li', attrs={"class": "topiclist topics with_categories"}):
+    print (deal.text.encode('utf-8'))
