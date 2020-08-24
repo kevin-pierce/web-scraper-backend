53138ad32b107aa148407c70ba1a1066ee102206
Kevin Pierce <kevinpierce08@gmail.com>
2020-08-24 14:36:00 -0400

    Now gathers data regarding shoe name, price, and image

--- webscraper.py
+++ webscraper.py
@@ -1,19 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://forums.redflagdeals.com/hot-deals-f9/?c=12"
-response = requests.get(url, timeout=5)
+url = "https://sneakernews.com/release-dates/"
+response = requests.get(url, timeout=20)
 soup = BeautifulSoup(response.content, "html.parser")
 
-deals = soup.findAll('li', attrs={"class": "row topic"})
+deals = soup.findAll('div', attrs={"class": "col lg-12"})
+#allShoes = deals.find('div', attrs={"class":"releases-box month-box col lg-2 sm-6 paged-1"})
+
+print(deals)
 
 todaysDeals = []
 
+dealObject = {
+    "shoeName":"",
+    "shoePrice":0,
+    "shoeImg":"",
+}
+
+shoeNames = []
+
 for deal in deals:
-    todaysDeals.append(deal);
-
-print(todaysDeals[0])
-#div div div div div div ul li'
-
-#for deal in soup.findAll('li', attrs={"class": "topiclist topics with_categories"}):
-#    print (deal.text.encode('utf-8'))
+    shoeContent = deal.find('div', attrs={"class":"content-box"})
+    
+    dealObject = {
+        "shoeName":shoeContent.find("h2").find("a").text,
+        "shoePrice":shoeContent.find("span", attrs={"class":"release-price"}).text,
+        "shoeImg":deal.find('div', attrs={"class":"image-box"}).find("a").find("img")['src']
+    }
+    print (dealObject)
