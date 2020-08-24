2083f4687e1df7ef182ccfe0f0bd302122bb4a1f
Kevin Pierce <kevinpierce08@gmail.com>
2020-08-24 13:20:46 -0400

    Refactored html parsing

--- webscraper.py
+++ webscraper.py
@@ -5,13 +5,14 @@ url = "https://forums.redflagdeals.com/hot-deals-f9/?c=12"
 response = requests.get(url, timeout=5)
 soup = BeautifulSoup(response.content, "html.parser")
 
-deals = soup.select('div#site_container div#site_content div#partition_forums div.forums_layout div.primary_content div.forumbg div.inner ul.topiclist.topics.with_categories')
+deals = soup.findAll('li', attrs={"class": "row topic"})
 
-#for deal in deals.findAll('ul', attrs={"class": "topiclist topics with_categories"}):
-#    print(deal)
+todaysDeals = []
 
-print (deals)
+for deal in deals:
+    todaysDeals.append(deal);
 
+print(todaysDeals[0])
 #div div div div div div ul li'
 
 #for deal in soup.findAll('li', attrs={"class": "topiclist topics with_categories"}):
