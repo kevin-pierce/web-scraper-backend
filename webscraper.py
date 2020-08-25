a7ac9323373ad4d3de1d1506a1730bbc3d035d44
Kevin Pierce <kevinpierce08@gmail.com>
2020-08-24 22:08:22 -0400

    Now acquires all release date for every shoe on the page

--- webscraper.py
+++ webscraper.py
@@ -5,22 +5,15 @@ url = "https://sneakernews.com/release-dates/"
 response = requests.get(url, timeout=20)
 soup = BeautifulSoup(response.content, "html.parser")
 
-deals = soup.findAll('div', attrs={"class": "col lg-12"})
+shoeReleases = soup.findAll('div', attrs={"class": "releases-box col lg-2 sm-6 paged-1"})
+
+print(len(shoeReleases))
+
 #allShoes = deals.find('div', attrs={"class":"releases-box month-box col lg-2 sm-6 paged-1"})
 
-print(deals)
+shoes = []
 
-todaysDeals = []
-
-dealObject = {
-    "shoeName":"",
-    "shoePrice":0,
-    "shoeImg":"",
-}
-
-shoeNames = []
-
-for deal in deals:
+for deal in shoeReleases:
     shoeContent = deal.find('div', attrs={"class":"content-box"})
     
     dealObject = {
@@ -29,4 +22,6 @@ for deal in deals:
         "shoeReleaseDate":shoeContent.find("div", attrs={"class":"release-date-and-rating"}).find("span", attrs={"class":"release-date"}).text.strip(),
         "shoeImg":deal.find('div', attrs={"class":"image-box"}).find("a").find("img")['src'],
     }
-    print (dealObject)
+    shoes.append(dealObject);
+
+print(shoes)
