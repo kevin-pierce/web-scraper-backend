ce540709d8646b232c43ff3e0a3c65b1d26d432c
Kevin Pierce <kevinpierce08@gmail.com>
2020-08-25 18:12:13 -0400

    Refactored API to scrape web on call

--- webscraper.py
+++ webscraper.py
@@ -4,33 +4,30 @@ from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
-url = "https://sneakernews.com/release-dates/"
-response = requests.get(url, timeout=20)
-soup = BeautifulSoup(response.content, "html.parser")
-
-shoeReleases = soup.findAll('div', attrs={"class": "releases-box col lg-2 sm-6 paged-1"})
-
-print(len(shoeReleases))
-
-#allShoes = deals.find('div', attrs={"class":"releases-box month-box col lg-2 sm-6 paged-1"})
-
-shoes = []
-
-for deal in shoeReleases:
-    shoeContent = deal.find('div', attrs={"class":"content-box"})
-    
-    dealObject = {
-        "shoeName":shoeContent.find("h2").find("a").text,
-        "shoePrice":shoeContent.find("span", attrs={"class":"release-price"}).text,
-        "shoeReleaseDate":shoeContent.find("div", attrs={"class":"release-date-and-rating"}).find("span", attrs={"class":"release-date"}).text.strip(),
-        "shoeImg":deal.find('div', attrs={"class":"image-box"}).find("a").find("img")['src'],
-    }
-    shoes.append(dealObject);
-
-print(shoes)
-
 @app.route('/shoepic/api/prod/v1.0/releases/all', methods=['GET'])
 def get_releases():
+
+    url = "https://sneakernews.com/release-dates/"
+    response = requests.get(url, timeout=20)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    shoeReleases = soup.findAll('div', attrs={"class": "releases-box col lg-2 sm-6 paged-1"})
+
+    print(len(shoeReleases))
+
+    shoes = []
+
+    for deal in shoeReleases:
+        shoeContent = deal.find('div', attrs={"class":"content-box"})
+        
+        dealObject = {
+            "shoeName":shoeContent.find("h2").find("a").text,
+            "shoePrice":shoeContent.find("span", attrs={"class":"release-price"}).text,
+            "shoeReleaseDate":shoeContent.find("div", attrs={"class":"release-date-and-rating"}).find("span", attrs={"class":"release-date"}).text.strip(),
+            "shoeImg":deal.find('div', attrs={"class":"image-box"}).find("a").find("img")['src'],
+        }
+        shoes.append(dealObject);
+
     return jsonify({'shoeData': shoes })
 
 if __name__ == '__main__':
