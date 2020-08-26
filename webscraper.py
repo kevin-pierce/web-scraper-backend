bff792854e9eede21a63f25a2ca4dc75db0515fc
Kevin Pierce <kevinpierce08@gmail.com>
2020-08-25 21:04:14 -0400

    Added sizeRun to the shoe object

--- webscraper.py
+++ webscraper.py
@@ -4,29 +4,34 @@ from flask import Flask, jsonify, abort
 
 app = Flask(__name__)
 
+url = "https://sneakernews.com/release-dates/"
+response = requests.get(url, timeout=20)
+soup = BeautifulSoup(response.content, "html.parser")
+
+shoeReleases = soup.findAll('div', attrs={"class": "releases-box col lg-2 sm-6 paged-1"})
+
+print(len(shoeReleases))
+
+shoes = []
+
+for deal in shoeReleases:
+    shoeContent = deal.find('div', attrs={"class":"content-box"})
+    shoeDetails = shoeContent.find("div", attrs={"class":"post-data"})
+    print(shoeDetails.findAll("p")[0].text[10:])
+    
+    dealObject = {
+        "sizeRun":shoeDetails.findAll("p")[0].find("span").text[10:],
+        "shoeName":shoeContent.find("h2").find("a").text,
+        "shoePrice":shoeContent.find("span", attrs={"class":"release-price"}).text,
+        "shoeReleaseDate":shoeContent.find("div", attrs={"class":"release-date-and-rating"}).find("span", attrs={"class":"release-date"}).text.strip(),
+        "shoeImg":deal.find('div', attrs={"class":"image-box"}).find("a").find("img")['src'],
+    }
+    shoes.append(dealObject);
+
 @app.route('/shoepic/api/prod/v1.0/releases/all', methods=['GET'])
 def get_releases():
 
-    url = "https://sneakernews.com/release-dates/"
-    response = requests.get(url, timeout=20)
-    soup = BeautifulSoup(response.content, "html.parser")
-
-    shoeReleases = soup.findAll('div', attrs={"class": "releases-box col lg-2 sm-6 paged-1"})
-
-    print(len(shoeReleases))
-
-    shoes = []
-
-    for deal in shoeReleases:
-        shoeContent = deal.find('div', attrs={"class":"content-box"})
-        
-        dealObject = {
-            "shoeName":shoeContent.find("h2").find("a").text,
-            "shoePrice":shoeContent.find("span", attrs={"class":"release-price"}).text,
-            "shoeReleaseDate":shoeContent.find("div", attrs={"class":"release-date-and-rating"}).find("span", attrs={"class":"release-date"}).text.strip(),
-            "shoeImg":deal.find('div', attrs={"class":"image-box"}).find("a").find("img")['src'],
-        }
-        shoes.append(dealObject);
+    
 
     return jsonify({'shoeData': shoes })
 
