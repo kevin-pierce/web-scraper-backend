7b6755d53927b0e8fcd6c30d913d71e499f3df94
Kevin Pierce <kevinpierce08@gmail.com>
2020-09-01 14:44:25 -0400

    Added scraping functionality for shoe image

--- webscraper.py
+++ webscraper.py
@@ -197,13 +197,19 @@ def scrape_nike_runner_sales(shoeReleaseDB, chromeOptions):
 
     for shoe in runnerSales:
         shoeDetails = shoe.find('div', attrs={"class":"product-card__info disable-animations"})
-        print(shoeDetails)
+        shoeImageData = shoe.find('a', attrs={"class":"product-card__img-link-overlay"})
+
+        print(shoeDetails.find('div', attrs={"class":"product-price is--current-price css-s56yt7"}).text)
+        print(shoeImageData.find('div', attrs={"class":"image-loader css-zrrhrw product-card__hero-image is--loaded"}))
+        print(shoeImageData.find('div', attrs={"class":"image-loader css-zrrhrw product-card__hero-image is--loaded"}).find("source", attrs={"srcset":True}))
 
         nikeRunnerObject = {
             "shoeName":shoeDetails.find('div', attrs={"class":"product-card__title"}).text,
-            "shoePrice":shoeDetails.find('div', attrs={"class":"product-price is--current-price css-s56yt7"}).text
+            "shoePrice":shoeDetails.find('div', attrs={"class":"product-price is--current-price css-s56yt7"}).text,
+            "shoeImg":shoeImageData.find('div', attrs={"class":"image-loader css-zrrhrw product-card__hero-image is--loaded"}).find("source", attrs={"srcset":True})
         }
-        print(nikeRunnerObject)
+
+    
 
     print(len(runnerSales))
 
